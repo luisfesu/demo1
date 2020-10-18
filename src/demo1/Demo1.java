aeaa501d0a0824647477857d06b6245c1f9eb722
uo271430 <73078102+uo271430@users.noreply.github.com>
2020-10-18 22:24:28 +0200

    Update Demo1.java

--- src/demo1/Demo1.java
+++ src/demo1/Demo1.java
@@ -3,7 +3,7 @@ package demo1;
 public class Demo1 {
 	
 	public static void main(String[] args) {
-		System.out.println("linea1");
+		System.out.println("linea1 SEUNDA MODIFICACION DESDE OTRO USUARIO");
 		System.out.println("linea2");
 		System.out.println("linea3 MODIFICACION DESDE OTRO USUARIO");
 	}
