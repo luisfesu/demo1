e06d246be89c7c657700e8e21dd150ebc01cb2df
Luis A. Fernández <58345609+luisfesu@users.noreply.github.com>
2020-10-18 22:49:47 +0200

    cambios desde usuario1 (ejer7)

--- src/demo1/Demo1.java
+++ src/demo1/Demo1.java
@@ -4,6 +4,7 @@ public class Demo1 {
 	
 	public static void main(String[] args) {
 		System.out.println("linea1 SEUNDA MODIFICACION DESDE OTRO USUARIO");
+<<<<<<< Upstream, based on origin/cambio2
 		System.out.println("linea2 EJER 7  modificacion desde usuario2");
 		System.out.println("linea3 MODIFICACION DESDE OTRO USUARIO");
 		System.out.println("linea4");
@@ -23,6 +24,9 @@ public class Demo1 {
 		System.out.println("cambios hechos en rama cambio2 desde primer usuario");
 		System.out.println("linea1 SEUNDA MODIFICACION DESDE OTRO USUARIO");
 		System.out.println("linea2");
+=======
+		System.out.println("linea2 cambios desde usuario1");
+>>>>>>> a1afd9c cambios desde usuario1 (ejer7)
 		System.out.println("linea3 MODIFICACION DESDE OTRO USUARIO");
 		System.out.println("linea4");
 		System.out.println("modificacion usuario 2 para ejer 6");
