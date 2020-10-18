2598db6a8f99f910dccbeb86a72c337c3e0ea689
Luis A. Fernández <58345609+luisfesu@users.noreply.github.com>
2020-10-18 22:56:44 +0200

    CARGA U1

--- src/demo1/Demo1.java
+++ src/demo1/Demo1.java
@@ -4,11 +4,7 @@ public class Demo1 {
 	
 	public static void main(String[] args) {
 		System.out.println("linea1 SEUNDA MODIFICACION DESDE OTRO USUARIO");
-<<<<<<< HEAD
-		System.out.println("linea2 cambios desde usuario1");
-=======
-		System.out.println("linea2 EJER 7  modificacion desde usuario2");
->>>>>>> refs/remotes/origin/master
+		System.out.println("linea2 cambios SOLUCIONADO");
 		System.out.println("linea3 MODIFICACION DESDE OTRO USUARIO");
 		System.out.println("linea4");
 		System.out.println("modificacion usuario 2 para ejer 6");
