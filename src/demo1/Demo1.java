d34ba8db09d5d6b4d0d4590ba46e28d7a350692f
Luis A. Fernández <58345609+luisfesu@users.noreply.github.com>
2020-10-18 22:03:18 +0200

    modificacion desde usuario2

--- src/demo1/Demo1.java
+++ src/demo1/Demo1.java
@@ -5,7 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("linea1");
 		System.out.println("linea2");
-		System.out.println("linea3");
+		System.out.println("linea3 MODIFICACION DESDE OTRO USUARIO");
 	}
 	
 }
