7ef1aa0ed5d90ade2339cbd489658fa7f0605294
Luis A. Fernández <58345609+luisfesu@users.noreply.github.com>
2020-10-18 21:05:59 +0200

    actualizacion Demo1 y craecion Demo2

--- src/demo1/Demo1.java
+++ src/demo1/Demo1.java
@@ -4,8 +4,8 @@ public class Demo1 {
 	
 	public static void main(String[] args) {
 		System.out.println("linea1");
-		System.out.println("linea1");
-		System.out.println("linea1");
+		System.out.println("linea2");
+		System.out.println("linea3");
 	}
 	
 }
