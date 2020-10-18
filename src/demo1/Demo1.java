333632bfb6a364dd3a5af9fdec18041663aca4f3
Luis A. Fernández <58345609+luisfesu@users.noreply.github.com>
2020-10-18 23:59:38 +0200

    canbios finalizados en rama3

--- src/demo1/Demo1.java
+++ src/demo1/Demo1.java
@@ -5,8 +5,6 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("linea1 SEUNDA MODIFICACION DESDE OTRO USUARIO");
 		System.out.println("linea2 cambios SOLUCIONADO");
-<<<<<<< HEAD
-=======
 		System.out.println("linea3 MODIFICACION DESDE OTRO USUARIO");
 		System.out.println("linea4");
 		System.out.println("modificacion usuario 2 para ejer 6");
@@ -25,7 +23,6 @@ public class Demo1 {
 		System.out.println("cambios hechos en rama cambio2 desde primer usuario");
 		System.out.println("linea1 SEUNDA MODIFICACION DESDE OTRO USUARIO");
 		System.out.println("linea2");
->>>>>>> branch 'cambio3' of https://github.com/luisfesu/demo1.git
 		System.out.println("linea3 MODIFICACION DESDE OTRO USUARIO");
 		System.out.println("linea4");
 		System.out.println("modificacion usuario 2 para ejer 6");
