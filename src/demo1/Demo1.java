4a862a9ee672dde694444490d5591743610a5b02
Luis A. Fernández <58345609+luisfesu@users.noreply.github.com>
2020-10-18 22:06:07 +0200

    modificacion hecha en linea2

--- src/demo1/Demo1.java
+++ src/demo1/Demo1.java
@@ -4,7 +4,7 @@ public class Demo1 {
 	
 	public static void main(String[] args) {
 		System.out.println("linea1");
-		System.out.println("linea2");
+		System.out.println("linea2 	MODIFICACION DESDE USUARIO1");
 		System.out.println("linea3");
 	}
 	
